dd9b9a27feec0f6c8c9a6d4b90ac9174b75ecd25
tsiresymila <tsiresymila@gmail.com>
2024-04-29 16:44:24 +0300

    Add async load

--- example/app_module.py
+++ example/app_module.py
@@ -1,4 +1,5 @@
 from nestipy.common import Module
+from nestipy_config import ConfigModule, ConfigOption, ConfigService
 
 from app_controller import AppController
 from app_service import AppService
@@ -6,17 +7,33 @@ from nestipy_peewee import PeeweeConfig, PeeweeModule
 from src.user.user_module import UserModule
 
 
+async def peewee_factory(config: ConfigService) -> PeeweeConfig:
+    return PeeweeConfig(
+        driver='mysql',
+        host=config.get('DB_HOST'),
+        user=config.get('DB_USER'),
+        password=config.get('DB_PASSWORD'),
+        port=int(config.get('DB_PORT')),
+        database=config.get('DB_DATABASE')
+    )
+
+
 @Module(
     imports=[
-        PeeweeModule.for_root(
-            PeeweeConfig(
-                driver='mysql',
-                host='localhost',
-                user='root',
-                password='',
-                port=3306,
-                database='nestipy'
-            )
+        ConfigModule.for_root(ConfigOption(), {'is_global': True}),
+        # PeeweeModule.for_root(
+        #     PeeweeConfig(
+        #         driver='mysql',
+        #         host='localhost',
+        #         user='root',
+        #         password='',
+        #         port=3306,
+        #         database='nestipy'
+        #     )
+        # ),
+        PeeweeModule.for_root_async(
+            factory=peewee_factory,
+            inject=[ConfigService]
         ),
         UserModule
     ],
